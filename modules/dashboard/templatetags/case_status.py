cb9fa21c15d74ff7d4e197811939f1c7140d087d
Dj-Denis <dj.denis97@gmail.com>
2018-07-24 14:20:47 +0300

    Fix case status message

--- modules/dashboard/templatetags/case_status.py
+++ modules/dashboard/templatetags/case_status.py
@@ -7,15 +7,16 @@ register = template.Library()
 
 @register.simple_tag
 def case_status(request):
-    cases = request.user.case_set.all()
-    for case in cases:
-        try:
-            logs = case.last_two_logs
-            if logs[0].status != logs[1].status:
-                if logs[1].status == '1':
-                    print(2)
-                    messages.add_message(request, messages.WARNING,
-                                         format_html("Кейс <a href='{}' class='alert-warning-link'> {}</a> закончился с ошибкой".format(
-                                             logs[1].case.get_absolute_url(), logs[1].case.name)))
-        except IndexError:
-            pass
+    if request.user.is_authenticated:
+        cases = request.user.case_set.all()
+        for case in cases:
+            try:
+                logs = case.last_two_logs
+                if logs[0].status != logs[1].status:
+                    if logs[1].status == '1':
+                        print(2)
+                        messages.add_message(request, messages.WARNING,
+                                             format_html("Кейс <a href='{}' class='alert-warning-link'> {}</a> закончился с ошибкой".format(
+                                                 logs[1].case.get_absolute_url(), logs[1].case.name)))
+            except IndexError:
+                pass
